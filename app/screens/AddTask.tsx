8c334033330e510a863545dbcb3d2eaae2812ca9
Andrew Capaldo <andycapaldo2@gmail.com>
2024-03-11 17:07:30 -0600

    Added firebase config, sends task data to firebase back-end db

--- app/screens/AddTask.tsx
+++ app/screens/AddTask.tsx
@@ -3,6 +3,10 @@ import { View, Text, TextInput, StyleSheet, TouchableOpacity } from 'react-nativ
 import CategorySelector from '../components/CategorySelector';
 import DueDateSelector from '../components/DueDateSelector';
 import Checkbox from 'expo-checkbox';
+import { FIRESTORE_DB } from '../../FirebaseConfig';
+import { addDoc, collection } from 'firebase/firestore';
+import { NavigationProp } from '@react-navigation/native';
+import { getAuth } from '../../FirebaseConfig';
 
 
 export const styles = StyleSheet.create({
@@ -107,12 +111,41 @@ export const styles = StyleSheet.create({
   }
 });
 
+interface AddTaskProps {
+  navigation: NavigationProp<any, any>;
+}
 
-const AddTask = () => {
 
+const AddTask = ( {navigation}: AddTaskProps ) => {
+  const auth = getAuth(); // Gets the authentication instance
+  const user = auth.currentUser; // Gets the currently logged-in user
+  const [taskName, setTaskName] = useState('');
+  const [taskDescription, setTaskDescription] = useState('')
+  const [category, setCategory] = useState('');
+  const [dueDate, setDueDate] = useState(''); // Still need to create functions for these two components
   const [isChecked, setChecked] = useState(false)
-  const buttonPressed = () => {
-    console.log('add task!')
+
+  const buttonPressed = async () => {
+    if (!taskName || !taskDescription) {
+      alert('Please fill in all fields');
+      return;
+    }
+    console.log('Adding task!');
+    try {
+      await addDoc(collection(FIRESTORE_DB, 'tasks'), {
+        taskName: taskName,
+        taskDescription: taskDescription,
+        category: category,
+        dueDate: dueDate,
+        highPriority: isChecked,
+        userId: user!.uid,
+      });
+      console.log('Task added!');
+      navigation.navigate('Dashboard')
+      alert('task added!')
+    } catch (error) {
+      console.error('Error adding task: ', error);
+    }
   }
 
   return (
@@ -124,7 +157,12 @@ const AddTask = () => {
         <Text style={styles.taskName}>Task Name</Text>
       </View>
       <View style={styles.nameInputContainer}>
-        <TextInput style={styles.nameInput} placeholder='Add a task' placeholderTextColor='#888' />
+        <TextInput 
+        style={styles.nameInput} 
+        placeholder='Add a task' 
+        placeholderTextColor='#888' 
+        value={taskName}
+        onChangeText={setTaskName}/>
       </View>
       <View style={styles.taskNameContainer}>
         <Text style={styles.taskName}>Task Description</Text>
@@ -132,10 +170,12 @@ const AddTask = () => {
       <View style={styles.descriptionInputContainer}>
         <TextInput 
         style={styles.descriptionInput}
-          placeholder='Description...'
-          placeholderTextColor='#888'
-          multiline={true} 
-          numberOfLines={4}/>
+        placeholder='Description...'
+        placeholderTextColor='#888'
+        multiline={true} 
+        numberOfLines={4}
+        value={taskDescription}
+        onChangeText={setTaskDescription}/>
       </View>
       <View style={styles.taskNameContainer}>
         <Text style={styles.taskName}>Category</Text>
