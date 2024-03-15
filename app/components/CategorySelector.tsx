52f326cef6db9b13169c26f83e2cf5012778f1e0
Andrew Capaldo <andycapaldo2@gmail.com>
2024-03-14 19:09:23 -0600

    AddTask now re-renders when a new category is added to display the new category

--- app/components/CategorySelector.tsx
+++ app/components/CategorySelector.tsx
@@ -1,5 +1,45 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { collection, getDocs, onSnapshot, query, where } from 'firebase/firestore';
+import { FIRESTORE_DB, getAuth } from '../../FirebaseConfig';
+
+interface Category {
+  name: string;
+  color: string;
+}
+
+
+
+const CategorySelector = () => {
+  const [categories, setCategories] = useState<Category[]>([]);
+  
+  useEffect(() => {
+    const auth = getAuth();
+    const user = auth.currentUser;
+    if (!user) return;
+
+    const q = query(collection(FIRESTORE_DB, 'categories'), where('userId', '==', user.uid));
+    
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      const categoryData: Category[] = querySnapshot.docs.map(doc => doc.data() as Category);
+      setCategories(categoryData);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+    return (
+    <View style={styles.categoryContainer}>
+        {categories.map((category, index) => (
+        <TouchableOpacity key={index} style={styles.categoryButton}>
+            <Text style={styles.categoryText}>{category.name}</Text>
+        </TouchableOpacity>
+        ))}
+    </View>
+    );
+};
+
+export default CategorySelector;
 
 const styles = StyleSheet.create ({ 
   categoryContainer: {
@@ -22,40 +62,3 @@ categoryText: {
   fontSize: 16
 }
 });
-
-const CategorySelector = () => {
-    // Placeholder state for categories
-    const categories = [
-    { id: '1', name: 'School' },
-    { id: '2', name: 'Finances' },
-    { id: '3', name: 'Home' },
-      // ... more placeholder categories
-    ];
-
-    // Placeholder useEffect and function to fetch categories from the database and update state
-  // useEffect(() => {
-  //   fetchCategories();
-  // }, []);
-
-  // const fetchCategories = async () => {
-  //   try {
-  //     let response = await fetch('/path-to-your-api');
-  //     let json = await response.json();
-  //     setCategories(json.categories);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
-
-    return (
-    <View style={styles.categoryContainer}>
-        {categories.map((category) => (
-        <TouchableOpacity key={category.id.toString()} style={styles.categoryButton}>
-            <Text style={styles.categoryText}>{category.name}</Text>
-        </TouchableOpacity>
-        ))}
-    </View>
-    );
-};
-
-export default CategorySelector;
