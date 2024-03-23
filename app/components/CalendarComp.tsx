5a25a2696ca074024626a5e6ebbc0224f6d44961
Andrew Capaldo <andycapaldo2@gmail.com>
2024-03-22 18:49:14 -0600

    Added styling for Calendar

--- app/components/CalendarComp.tsx
+++ app/components/CalendarComp.tsx
@@ -1,12 +1,31 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import { Calendar } from 'react-native-calendars'
 
 
 const CalendarComp = () => {
   return (
-    <View>
-      <Calendar onDayPress={day => {console.log('selected day', day);}} />
+    <View style={styles.centeredView}>
+      <Calendar 
+      onDayPress={day => {console.log('selected day', day);}} 
+      style={{height: 650, width: 400}}  
+      theme={{ 
+        backgroundColor: '#111111', 
+        calendarBackground: '#111111', 
+        textSectionTitleColor: '#FEFEFE',
+        dayTextColor: '#FEFEFE',
+        monthTextColor: '#FEFEFE',
+        indicatorColor: 'black',
+        }} />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: '#111111'
+  },
+})
 export default CalendarComp
