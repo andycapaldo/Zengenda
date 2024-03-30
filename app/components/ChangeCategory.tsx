4134f1ede9995c7af7d87dbd499e501f7e7b22b4
Andrew Capaldo <andycapaldo2@gmail.com>
2024-03-30 16:13:14 -0600

    Added category delete function which cascade deletes all associated tasks and the category itself from firestore

--- app/components/ChangeCategory.tsx
+++ app/components/ChangeCategory.tsx
@@ -6,11 +6,36 @@ import {
     MenuTrigger,
 } from 'react-native-popup-menu';
 import { Entypo } from '@expo/vector-icons';
+import { collection, deleteDoc, doc, getDocs, query, where, writeBatch } from 'firebase/firestore';
+import { FIRESTORE_DB } from '../../FirebaseConfig';
 
 
 
 export default function ChangeCategory({ category }) {
 
+    const deleteCategory = async () => {
+        const taskQuery = query(
+            collection(FIRESTORE_DB, 'tasks'),
+            where("category", "==", doc(FIRESTORE_DB, "categories", category.id))
+        );
+
+        try {
+            const taskQuerySnapshot = await getDocs(taskQuery);
+
+            const batch = writeBatch(FIRESTORE_DB);
+
+            taskQuerySnapshot.forEach((doc) => {
+                batch.delete(doc.ref)
+            });
+
+            await batch.commit();
+
+            await deleteDoc(doc(FIRESTORE_DB, "categories", category.id));
+            console.log(`Category and assoicated tasks deleted for ${category.name}`);
+        } catch (error) {
+        console.error('Error deleting category and associated tasks: ', error);
+        } 
+    };
 
 
 return (
@@ -21,7 +46,7 @@ return (
             </MenuTrigger>
             <MenuOptions>
                 <MenuOption onSelect={() => console.log(`Edit button pressed for ${category.name}`)} text='Edit' />
-                <MenuOption onSelect={() => console.log(`Delete button pressed for ${category.name}`)} text='Delete' />
+                <MenuOption onSelect={() => deleteCategory()} text='Delete' />
             </MenuOptions>
         </Menu>
     </View>
