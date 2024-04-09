58e9fcaa27eb6939f3d8ed373cc30e81cba10b8c
Andrew Capaldo <andycapaldo2@gmail.com>
2024-04-08 19:26:54 -0600

    Changed styling, added more inspirational quotes

--- app/components/MotivationalQuotes.tsx
+++ app/components/MotivationalQuotes.tsx
@@ -7,7 +7,7 @@ import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
 export default function MotivationalQuotes() {
     const [selectedQuote, setSelectedQuote] = useState('');
 
-    const quotes = ['“Fear is what stops you. Courage is what keeps you going.” — Unknown', '“Have the courage to follow your heart and intuition. They somehow know what you truly want to become.” — Steve Jobs', "“Courage is found in unlikely places.” — J.R.R. Tolkien", "“Change is inevitable but personal growth is a choice.” — Bob Proctor", "“I am not what happened to me, I am what I choose to become.” — Carl Jung",]
+    const quotes = ['"Fear is what stops you. Courage is what keeps you going." — Unknown', '"Have the courage to follow your heart and intuition. They somehow know what you truly want to become." — Steve Jobs', '"Courage is found in unlikely places." — J.R.R. Tolkien', '"Change is inevitable but personal growth is a choice." — Bob Proctor', '"I am not what happened to me, I am what I choose to become." — Carl Jung', '"Genius is one percent inspiration and ninety-nine percent perspiration." — Thomas Edison', '"To be in harmony with the wholeness of things is not to have anxiety over imperfections." — Dogen', '"If you are depressed, you are living in the past. If you are anxious, you are living in the future. if you are at peace, you are living in the present." — Lao Tzu', '"The key to immortality is first living a life worth remembering." — Bruce Lee', '"The privilege of a lifetime is to become who you truly are." — Carl Jung', '"Stop being a prisoner of your past. Become the architect of your future." — Robin Sharma', '"No valid plans for the future can be made by those who have no capacity for living now." — Alan Watts', '"Learning is a gift, even when pain is your teacher." — Michael Jordan' ]
 
     useEffect(() => {
         selectQuote();
@@ -18,13 +18,11 @@ export default function MotivationalQuotes() {
         setSelectedQuote(quotes[random]); 
     }
 
-    console.log(selectedQuote)
 return (
     <View style={styles.rememberContainerWrapper}>
         <View style={styles.rememberContainer}>
             <View style={styles.remember}>
-                <Image
-                source={require("../components/images2/remember.png")} />
+                <Text style={styles.rememberText}>Remember:</Text>
             </View>
             <View style={styles.inspiration}>
                 <Text style={styles.quoteText}>{selectedQuote}</Text>
@@ -32,7 +30,7 @@ return (
             <TouchableOpacity style={styles.refresh} onPress={selectQuote}>
                 <View style={styles.repeatIcon}>
                     <Image 
-                        source={require("../components/images2/repeaticon.png")}
+                        source={require("../components/images2/shuffleButton.png")}
                         style={styles.refresh} 
                     />
                 </View>
@@ -58,11 +56,16 @@ const styles = StyleSheet.create ({
         flex: 1.8,
         justifyContent: "center",
         alignItems: "center",
-        borderRadius: 3,
+        borderRadius: 20,
     },
     remember: {
-        flex: 0.1,
+        flex: 0.2,
         paddingTop: 10,
+    },
+    rememberText: {
+        fontFamily: "Quicksand_400Regular",
+        fontSize: 35,
+        fontWeight: 'bold',
     },
     inspiration: {
         flex: 0.7,
